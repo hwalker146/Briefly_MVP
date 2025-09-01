d32a05fe1084e540c1d1cae80512f8621e051174
harrywalker146 <hwalker@harvard.college.edu>
2025-09-01 14:40:54 -0400

    Explicitly set redirect URI in Google provider config

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -6,6 +6,11 @@ const handler = NextAuth({
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      authorization: {
+        params: {
+          redirect_uri: 'https://briefly-mvp.vercel.app/api/auth/callback/google'
+        }
+      }
     }),
   ],
   pages: {
